4a1886f7f43414ce7dd22b8b2b10af83bfc66ffc
Sujata Mishra <mishrasujata1628@gmail.com>
2020-10-02 17:45:58 +0530

    Local Commits

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js");
 
 firebase.initializeApp({
-  messagingSenderId: "1062407524656"
+  messagingSenderId: "752278633076"
 });
 
 const messaging = firebase.messaging();
--- src/init-fcm.js
+++ src/init-fcm.js
@@ -1,14 +1,23 @@
 import * as firebase from "firebase/app";
 import "firebase/messaging";
 
-const initializedFirebaseApp = firebase.initializeApp({
-  messagingSenderId: "1062881024656"
-});
+var firebaseConfig = {
+  apiKey: "AIzaSyD2MH-cx9GMRoJ_M7fHxPPueTJBvKYoAAk",
+  authDomain: "push-notification-demo-f93ec.firebaseapp.com",
+  databaseURL: "https://push-notification-demo-f93ec.firebaseio.com",
+  projectId: "push-notification-demo-f93ec",
+  storageBucket: "push-notification-demo-f93ec.appspot.com",
+  messagingSenderId: "752278633076",
+  appId: "1:752278633076:web:036cca618a30fd5fd36f21",
+  measurementId: "G-QF4S0XMG33"
+};
+
+const initializedFirebaseApp = firebase.initializeApp(firebaseConfig);
 
 const messaging = initializedFirebaseApp.messaging();
 
 messaging.usePublicVapidKey(
-  "BD6n7ebJq...rP-ytiqlsj4AcNNeQcbes"
+  "BEz3gZk5835_7KQRC8wxu0FibbdmYUSbZVFfVfRq3pJylj9OcWDB0MZSXKEAm9W6tcio66LpmuZNg3SvmQIvVhg"
 );
 
 export { messaging };
